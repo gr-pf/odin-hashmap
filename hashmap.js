ba6dc98701aa147279b5042b88b18735efafda88
gr-pf <237513179+gr-pf@users.noreply.github.com>
2026-05-29 16:55:18 +0200

    Init HM class

---
+++ hashmap.js
@@ -0,0 +1,29 @@
+class HashMap {
+  constructor(loadFactor = 0.75, capacity = 16) {
+    this.loadFactor = loadFactor;
+    this.capacity = capacity;
+    this.length = 0;
+  }
+
+  /**
+   *
+   * @param {String} key
+   * @returns {Number}
+   */
+  hash(key) {
+    if (typeof key !== "string") {
+      throw new TypeError(`key must be a string: ${key} is a ${typeof key}`);
+    }
+
+    let hashCode = 0;
+
+    const primeNumber = 31;
+    for (let i = 0; i < key.length; i++) {
+      hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % this.capacity;
+    }
+
+    return hashCode;
+  }
+
+  set(key, value) {}
+}
